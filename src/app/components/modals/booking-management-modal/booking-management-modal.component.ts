fa43ecfe25691b8653b7cb2115b6591ca6255f8f
CristianRivera03 <cr8539788@gmail.com>
2026-04-20 06:03:50 -0600

    feat: add booking management modal component with template

---
+++ src/app/components/modals/booking-management-modal/booking-management-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-management-modal',
+  imports: [],
+  templateUrl: './booking-management-modal.component.html',
+})
+export class BookingManagementModalComponent {
+
+}
